bb1e3669d7ca523c7dd1d1ef6bbf2916e3c376ee
KostisGrf <34042957+KostisGrf@users.noreply.github.com>
2020-11-12 11:43:11 +0200

    Adds project

---
+++ Reception  System/src/Home/Controller.java
@@ -0,0 +1,112 @@
+package Home;
+
+import java.awt.*;
+import java.io.IOException;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TextField;
+
+
+
+
+public class Controller {
+    //menu
+    public Button NKbutton=new Button();
+    public Button updateButton=new Button();
+    public Button deleteButton=new Button();
+    public Button SEbutton=new Button();
+
+    //nea krathsh
+    public TextField txtfield=new TextField();
+
+
+
+
+
+
+
+    public void mouseEnter(){
+        NKbutton.setStyle("-fx-background-color: #6a25cc;");
+
+    }
+    public void mouseExit(){
+        NKbutton.setStyle("-fx-background-color:  #3F2B63;");}
+
+    public void mouseEnter2(){
+        updateButton.setStyle("-fx-background-color: #6a25cc;");
+
+    }
+    public void mouseExit2(){
+        updateButton.setStyle("-fx-background-color:  #3F2B63;");}
+
+    public void mouseEnter3(){
+        deleteButton.setStyle("-fx-background-color: #6a25cc;");
+
+    }
+    public void mouseExit3(){
+        deleteButton.setStyle("-fx-background-color:  #3F2B63;");}
+
+        public void mouseEnter4(){
+        SEbutton.setStyle("-fx-background-color: #6a25cc;");
+
+    }
+    public void mouseExit4(){
+        SEbutton.setStyle("-fx-background-color:  #3F2B63;");}
+
+
+    public void prints(){
+        String textfieldValue=txtfield.getText();
+
+        System.out.println(textfieldValue);}
+
+        public void onclickhndle(ActionEvent event)throws IOException {
+            String evt=((Button) event.getSource()).getId();
+
+            Parent rootparent= FXMLLoader.load(getClass().getResource("fxml/Μain.fxml"));
+
+            Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+
+
+
+
+        switch (evt){
+            case "NKbutton":rootparent=FXMLLoader.load(getClass().getResource("fxml/NeaKrathsh.fxml"));
+
+                break;
+            case "updateButton":rootparent= FXMLLoader.load(getClass().getResource("fxml/Update.fxml"));
+                break;
+            case "deleteButton":rootparent= FXMLLoader.load(getClass().getResource("fxml/Delete.fxml"));
+                break;
+            case "SEbutton":rootparent = FXMLLoader.load(getClass().getResource("fxml/SE.fxml"));
+                break;
+
+            
+
+        }   Scene scene=new Scene(rootparent);
+            window.setScene(scene);
+            window.show();
+
+
+        }
+
+
+
+}
+
+
