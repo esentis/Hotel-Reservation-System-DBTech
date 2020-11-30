0d48ca946accc4da4b5d14113b9b3ecc1db93e99
KostisGrf <34042957+KostisGrf@users.noreply.github.com>
2020-11-30 21:41:00 +0200

    Update Staff.java

--- Reception  System/src/Home/Staff.java
+++ Reception  System/src/Home/Staff.java
@@ -1,46 +1,92 @@
 package Home;
 
 public class Staff {
-    private String firstname;
-    private String lasstname;
+    private long Id;
+    private String FirstName;
+    private String LastName;
+    private String UserName;
     private String email;
-    private String username;
     private String password;
-    private Long phonenumber;
+    private long PhoneNumber;
+    private long RoleId;
 
-    public Staff() {
-    }
 
-    public Staff(String firstname, String lasstname, String email, String username, String password, Long phonenumber) {
-        this.firstname = firstname;
-        this.lasstname = lasstname;
+    public Staff(){}
+
+    
+
+    public Staff(long id, String firstName, String lastName, String userName, String email, String password, long phoneNumber, long roleId) {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        UserName = userName;
         this.email = email;
-        this.username = username;
         this.password = password;
-        this.phonenumber = phonenumber;
+        PhoneNumber = phoneNumber;
+        RoleId = roleId;
     }
 
-    public String getFirstname() {
-        return firstname;
+    public long getId() {
+        return Id;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+    public String getFirstName() {
+        return FirstName;
     }
 
-    public String getLasstname() {
-        return lasstname;
+    public String getLastName() {
+        return LastName;
     }
 
-    public void setLasstname(String lasstname) {
-        this.lasstname = lasstname;
+    public String getUserName() {
+        return UserName;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public long getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public long getRoleId() {
+        return RoleId;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public void setRoleId(long roleId) {
+        RoleId = roleId;
     }
 }
